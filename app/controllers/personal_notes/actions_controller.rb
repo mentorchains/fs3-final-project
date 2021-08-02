160251f83d42e4efec20871c4bf44c24a78af862
laurenmacpherson <lauren.macpherson@sjsu.edu>
2021-08-02 16:23:27 -0700

    more changes, mostly comments

--- app/controllers/personal_notes/actions_controller.rb
+++ app/controllers/personal_notes/actions_controller.rb
@@ -4,19 +4,35 @@ module PersonalNotes
 
     before_action :ensure_logged_in
 
-    def index
-      Rails.logger.info 'Called NotesController#index'
-      notes = NoteStore.get_notes()
-  
-      render json: { notes: notes.values }
-    end
-
-    #BELOW: 
-    #This is the original actions_controller I found from frontend
+    #Below: rendering notes on load?
     #def index
-    #  render_json_dump({ actions: [] })
+    #  Rails.logger.info 'Called NotesController#index'
+    #  notes = NoteStore.get_notes()
+  
+    #  render json: { notes: notes.values }
     #end
 
+    def index
+      render_json_dump({ actions: [] })
+    end
+
+    def update 
+      Rails.logger.info 'Called NotesController#update'
+
+      note_id = params[:note_id]
+      note = {
+        'id' => note_id,
+        'content' => params[:note][:content]
+      } 
+      #Content refers to what's written in the text box, what is 'note'?
+      #Is that the object?
+
+      NoteStore.add_note(note_id, note)
+
+      render json: { note: note }
+    end
+
+    #should also have a way to delete?
 
     #Does this show a list of notes?
     def show
